dcadb005df588cb7f0350057188a43c240fd36a6
Ricardo Perez <jricardo27@gmail.com>
2025-02-07 19:50:11 +1100

    Factor out StyleGeoJson

--- src/components/MapComponent/FeatureMap.tsx
+++ src/components/MapComponent/FeatureMap.tsx
@@ -1,15 +1,12 @@
 import L from "leaflet"
 import React, { useMemo, useCallback, useEffect, useState } from "react"
-import { createRoot } from "react-dom/client"
-import { GeoJSON } from "react-leaflet"
 
 import MapComponent, { LayerGroupChild, MapComponentProps } from "../../components/MapComponent/MapComponent.tsx"
-import PopupContent from "../../components/PopupContent/PopupContent.tsx"
 import SavedFeaturesDrawer from "../../components/SavedFeaturesDrawer/SavedFeaturesDrawer.tsx"
 import { GeoJsonFeature } from "../../data/types"
 import { TTabMapping } from "../../data/types/TTabMapping.ts"
 import useGeoJsonMarkers from "../../hooks/useGeoJsonMarkers.ts"
-import createCustomIcon from "../../utils/createCustomIcon.tsx"
+import StyledGeoJson, { contextMenuHandlerProps } from "../StyledGeoJson/StyledGeoJson.tsx"
 
 import FeatureMapContextMenu from "./FeatureMapContextMenu.tsx"
 
@@ -25,12 +22,10 @@ export const FeatureMap = ({ geoJsonOverlaySources, ...mapProps }: FeatureMapPro
   const overlayFilePaths = useMemo(() => (Object.keys(geoJsonOverlaySources)), [geoJsonOverlaySources])
   const overlayMarkers = useGeoJsonMarkers(overlayFilePaths)
 
-  const pointToLayer = useCallback((feature: GeoJsonFeature, latlng: unknown) => {
-    const iconName = feature.properties.style?.icon || "fa/FaMapMarker"
-    const iconColor = feature.properties.style?.color || "grey"
-    const innerIconColor = feature.properties.style?.innerIconColor || iconColor
-    const customIcon = createCustomIcon(iconName, iconColor, innerIconColor)
-    return L.marker(latlng, { icon: customIcon })
+  const onContextMenuHandler = useCallback(({ event, feature }: contextMenuHandlerProps) => {
+    L.DomEvent.stopPropagation(event)
+    setContextMenuPosition(event.latlng)
+    setSelectedFeature(feature)
   }, [])
 
   useEffect(() => {
@@ -43,47 +38,18 @@ export const FeatureMap = ({ geoJsonOverlaySources, ...mapProps }: FeatureMapPro
           key: layerName,
           title: layerName,
           children: (
-            <GeoJSON
+            <StyledGeoJson
               data={data}
-              pointToLayer={pointToLayer}
-              onEachFeature={(feature, layer) => {
-                if (feature.properties) {
-                  if (feature.properties.name) {
-                    layer.bindTooltip(`${feature.properties.name}`, { permanent: false, direction: "auto" })
-                  }
-
-                  layer.bindPopup("", { minWidth: 900, maxHeight: 500, keepInView: true }) // Initialize with empty content
-
-                  // Add an event listener for when the popup opens
-                  layer.on("popupopen", () => {
-                    // Get the popup element
-                    const popup = layer.getPopup()
-                    if (popup) {
-                      // Create a container for the popup content
-                      const container = L.DomUtil.create("div")
-                      const root = createRoot(container)
-                      root.render(
-                        <PopupContent feature={feature as GeoJsonFeature} tabMapping={tabMapping} />,
-                      )
-
-                      // Set the popup content
-                      popup.setContent(container)
-                    }
-                  })
-
-                  layer.on("contextmenu", (event) => {
-                    L.DomEvent.stopPropagation(event)
-                    setContextMenuPosition(event.latlng)
-                    setSelectedFeature(feature as GeoJsonFeature)
-                  })
-                }
-              }}
+              popupTabMapping={tabMapping}
+              contextMenuHandler={onContextMenuHandler}
+              popupProps={{ minWidth: 900, maxHeight: 500, keepInView: true }}
             />
           ),
         }
-      }))
+      },
+      ))
     }
-  }, [geoJsonOverlaySources, overlayMarkers, pointToLayer])
+  }, [geoJsonOverlaySources, overlayMarkers, onContextMenuHandler])
 
   return (
     <>
---
+++ src/components/StyledGeoJson/StyledGeoJson.tsx
@@ -0,0 +1,91 @@
+import L from "leaflet"
+import React, { useCallback } from "react"
+import { createRoot } from "react-dom/client"
+import { GeoJSON } from "react-leaflet"
+
+import { GeoJsonFeature, GeoJsonCollection } from "../../data/types"
+import { TTabMapping } from "../../data/types/TTabMapping.ts"
+import createCustomIcon from "../../utils/createCustomIcon.tsx"
+import PopupContent from "../PopupContent/PopupContent.tsx"
+
+export interface onPopupOpenProps {
+  feature: GeoJsonFeature
+  layer: L.Layer
+  popupTabMapping: TTabMapping
+}
+
+export interface contextMenuHandlerProps {
+  event: L.Event
+  feature: GeoJsonFeature
+}
+
+export interface popupProps {
+  minWidth: number
+  maxHeight: number
+  keepInView: boolean
+}
+
+interface StyleGeoJsonProps {
+  data: GeoJsonCollection | null
+  popupTabMapping: TTabMapping
+  popupOpenHandler?: ({ feature, layer, popupTabMapping }: onPopupOpenProps) => void
+  contextMenuHandler?: ({ event, feature }: contextMenuHandlerProps) => void
+  popupProps?: popupProps
+}
+
+const StyledGeoJson = ({ data, popupTabMapping, popupOpenHandler, contextMenuHandler, popupProps }: StyleGeoJsonProps): React.ReactNode => {
+  const pointToLayer = useCallback((feature: GeoJsonFeature, latlng: unknown) => {
+    const iconName = feature.properties.style?.icon || "fa/FaMapMarker"
+    const iconColor = feature.properties.style?.color || "grey"
+    const innerIconColor = feature.properties.style?.innerIconColor || iconColor
+    const customIcon = createCustomIcon(iconName, iconColor, innerIconColor)
+    return L.marker(latlng, { icon: customIcon })
+  }, [])
+
+  const defaultOnPopupOpen = useCallback(({ feature, layer, popupTabMapping }: onPopupOpenProps) => {
+    // Get the popup element
+    const popup = layer.getPopup()
+    if (popup) {
+      // Create a container for the popup content
+      const container = L.DomUtil.create("div")
+      const root = createRoot(container)
+      root.render(
+        <PopupContent feature={feature as GeoJsonFeature} tabMapping={popupTabMapping} />,
+      )
+
+      // Set the popup content
+      popup.setContent(container)
+    }
+  }, [])
+
+  const onPopupOpenHandler = popupOpenHandler || defaultOnPopupOpen
+
+  return (
+    <GeoJSON
+      data={data}
+      pointToLayer={pointToLayer}
+      onEachFeature={(feature, layer) => {
+        if (feature.properties) {
+          if (feature.properties.name) {
+            layer.bindTooltip(`${feature.properties.name}`, { permanent: false, direction: "auto" })
+          }
+
+          layer.bindPopup("", { ...popupProps || {} }) // Initialize with empty content
+
+          // Add an event listener for when the popup opens
+          layer.on("popupopen", () => {
+            onPopupOpenHandler({ feature, layer, popupTabMapping })
+          })
+
+          if (contextMenuHandler) {
+            layer.on("contextmenu", (event) => {
+              contextMenuHandler({ event, feature })
+            })
+          }
+        }
+      }}
+    />
+  )
+}
+
+export default StyledGeoJson
